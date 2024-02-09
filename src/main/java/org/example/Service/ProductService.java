5250d9188f43a6c0ce804e9fb7c83307c580c385
cramster <87053198+Cramster@users.noreply.github.com>
2024-02-09 14:00:39 -0500

    updated ProductService to inject SellerService

--- src/main/java/org/example/Service/ProductService.java
+++ src/main/java/org/example/Service/ProductService.java
@@ -7,15 +7,31 @@ import java.util.List;
 
 public class ProductService {
 
+    SellerService sellerService;
+
     List<Product> productList;
-    public ProductService(){
+
+    public ProductService(SellerService sellerService){
+        this.sellerService = sellerService;
         productList = new ArrayList<>();
     }
 
-    public List<Product> getAllProducts(){
+    public List<Product> getProductList(){
         return productList;
     }
 
+    public void addProduct(Product p){
+        productList.add(p);
+    }
+
+
+//from 2-6 car service example, above being from painter/author example
+   public ProductService(){
+        productList = new ArrayList<>();
+    }
+    public List<Product> getAllProducts(){
+        return productList;
+    }
     public void insertProduct(Product product){
         productList.add(product);
     }
--- src/main/java/org/example/Service/SellerService.java
+++ src/main/java/org/example/Service/SellerService.java
@@ -1,2 +1,24 @@
-package org.example.Service;public class SellerService {
+package org.example.Service;
+
+import org.example.Model.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SellerService {
+
+    List<Seller> sellerList;
+
+    public SellerService(){
+        this.sellerList = new ArrayList<>();
+    }
+
+    public List<Seller> getSellerList(){
+        return sellerList;
+    }
+
+    public void addSeller(Seller s){
+        sellerList.add(s);
+    }
+
 }
